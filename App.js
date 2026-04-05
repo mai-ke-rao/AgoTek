6e159efeb4393e38a7a76b93860fc1ce6d0adfc8
Aleksandar T. <aleksandartomic420@gmail.com>
2026-04-05 22:29:47 +0200

    .

--- App.js
+++ App.js
@@ -36,7 +36,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
  cors: {
-    origin: "http://localhost:5173",
+    origin: "https://agotek.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
